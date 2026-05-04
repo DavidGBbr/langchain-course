b90b8fb4e3c93351b23d4c4d785f93fae8aebd05
DavidGBbr <davidgomesbrigido@gmail.com>
2026-05-04 09:32:21 -0300

    feat: ollama llm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-
+from langchain_ollama import ChatOllama
 load_dotenv()
 
 
@@ -25,7 +25,8 @@ def main():
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+    # llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
